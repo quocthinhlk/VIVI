00372e61cc33dd0c0605d1f25e0564eb1a5d906d
quocthinhlk <quocthinhlk@gmail.com>
2019-06-26 09:29:31 +0700

    Add CIBlock Bitrix

---
+++ CIBlock/add.php
@@ -0,0 +1,27 @@
+﻿<?
+$iblock = new CIBlock;
+$arFields = Array(
+  "ACTIVE" => 'Y',
+  "NAME" => 'Learning CIBlock',
+  "CODE" => 'LEARNGING CIBLOCK',
+  "LIST_PAGE_URL" => '',
+  "DETAIL_PAGE_URL" => '',
+  "IBLOCK_TYPE_ID" => 'lists',
+  "SITE_ID" => 's1',
+  "SORT" => 500,
+  "DESCRIPTION" => 'This is test CIBlock Bitrix24',
+  "DESCRIPTION_TYPE" => 'text',
+  "WORKFLOW"  =>  'N',
+  );
+if ($ID_Block > 0){
+  $res = $iblock->Update($ID_Block, $arFields);
+  echo 'Update Success ID '.$ID_Block;
+  AddMessage2Log($ID_Block,__LINE__.__FILE__);
+}
+else{
+  $ID_Block = $iblock->Add($arFields);
+  $res = ($ID_Block>0);
+  echo 'Add Success ID '.$ID_Block;
+  AddMessage2Log($ID_Block,__LINE__.__FILE__);
+}
+?>
