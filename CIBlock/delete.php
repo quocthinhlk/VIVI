b63de29645ff408b2642e2edf1078945b1956e23
quocthinhlk <quocthinhlk@gmail.com>
2019-06-26 10:04:06 +0700

    Delete CIBlock

---
+++ CIBlock/delete.php
@@ -0,0 +1,16 @@
+﻿<?
+$ID_Block_Delete = 346;
+if($USER->IsAdmin())
+{
+    $DB->StartTransaction();
+    if(!CIBlock::Delete($ID_Block_Delete))
+    {
+        $strWarning.=GetMessage("IBLOCK_DELETE_ERROR");
+        $Db->rollback();
+    }
+    else{
+      $DB->Commit();
+      echo 'Delele Success Iblock ID '.$ID_Block_Delete;
+    }
+}
+?>
