e1cd92fa95ce66e91a2f4cb939c9e29f727a26ea
quocthinhlk <quocthinhlk@gmail.com>
2019-06-28 09:06:52 +0700

    Update IBlock

---
+++ CIBlock/update.php
@@ -0,0 +1,23 @@
+﻿<? 
+$iblock = new CIBlock;
+$arFields = Array(
+	"ACTIVE" => 'Y',
+	"NAME" => 'Learning CIBlock',
+	"CODE" => 'LEARNGING CIBLOCK',
+	"LIST_PAGE_URL" => '',
+	"DETAIL_PAGE_URL" => '',
+	"IBLOCK_TYPE_ID" => 'lists',
+	"SITE_ID" => 's1',
+	"SORT" => 500,
+	"DESCRIPTION" => 'This is test CIBlock Bitrix24',
+	"DESCRIPTION_TYPE" => 'text',
+	"WORKFLOW"  =>  'N',
+);
+$ID_Block_Update = 352;
+if ($res = $iblock->Update($ID_Block_Update, $arFields)){
+	echo 'Update Success ID '.$ID_Block_Update;
+}
+else{
+	echo 'Update Fail ID '.$ID_Block_Update;
+}
+?>
