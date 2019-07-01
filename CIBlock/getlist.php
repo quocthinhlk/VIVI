bee5023c9d2485d1f1b2cc29f9b267da4703d277
quocthinhlk <quocthinhlk@gmail.com>
2019-07-01 15:52:21 +0700

    getlist CIBlock

---
+++ CIBlock/getlist.php
@@ -0,0 +1,12 @@
+﻿<?php
+if(!CModule::IncludeModule("iblock"))
+	return; 
+$iBlockCode = 'EQUIPMENT_LIST_WAREHOUSE';
+$iblockType = 'lists';
+$iBlockObject = CIBlock:: GetList(Array(), Array('CODE'=> $iBlockCode, 'TYPE' => $iblockType));
+while ($ar_res = $iBlockObject -> Fetch()) {
+	$IBlockID = $ar_res['ID'];
+	break;
+}
+var_dump($IBlockID);
+?>
